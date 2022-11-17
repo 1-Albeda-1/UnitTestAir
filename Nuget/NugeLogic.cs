6648aa0467273a992ee17cf07aacae1337264261
Кристина <106802110+1-Albeda-1@users.noreply.github.com>
2022-11-17 21:12:26 +0300

    Add files via upload

---
+++ Nuget/NugeLogic.cs
@@ -0,0 +1,31 @@
+﻿using UserStoryAir;
+using UserStoryAir.Models;
+
+namespace Nuget
+{
+        public class NugetLogic
+        {
+            private List<Reys> reyses = new List<Reys>();
+
+            public NugetLogic() { }
+
+            public List<Reys> GetList()
+            {
+                return reyses;
+            }
+            public void Add(Reys arg)
+            {
+                reyses.Add(arg);
+            }
+            public void Remove(Reys arg)
+            {
+                reyses.Remove(arg);
+            }
+            public void Change(Reys id, Reys arg)
+            {
+                var index = reyses.IndexOf(id);
+                reyses[index] = arg;
+            }
+        }
+    
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,88 @@
+using Xunit;
+using UserStoryAir;
+using UserStoryAir.Models;
+using Nuget;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+            [Fact]
+            public void AddButton()
+            {
+                NugetLogic RS = new();
+                var result = RS.GetList();
+                Reys mytest = new()
+                {
+                    NumberReys = 253,
+                    Plane = plane.Airbus,
+                    arrivalTime = System.DateTime.Now,
+                    NumberPassengers = 100,
+                    NumberCrew = 20,
+                    SborC = 1500,
+                    SborP = 2000,
+                    allowance = 3000,
+
+                };
+                RS.Add(mytest);
+                Assert.Equal(mytest, result[0]);
+            }
+
+            [Fact]
+            public void ChangeButton()
+            {
+            NugetLogic RS = new();
+            var result = RS.GetList();
+            Reys mytest = new()
+            {
+                NumberReys = 253,
+                Plane = plane.Airbus,
+                arrivalTime = System.DateTime.Now,
+                NumberPassengers = 100,
+                NumberCrew = 2,
+                SborC = 1500,
+                SborP = 2000,
+                allowance = 3000,
+
+            };
+            RS.Add(mytest);
+            Reys mytest2 = new()
+            {
+                NumberReys = 305,
+                Plane = plane.Airbus,
+                arrivalTime = System.DateTime.Now,
+                NumberPassengers = 170,
+                NumberCrew = 30,
+                SborC = 1500,
+                SborP = 2000,
+                allowance = 3000,
+
+            };
+            RS.Change(mytest, mytest2);
+                Assert.Equal(mytest2, result[0]);
+            }
+
+            [Fact]
+            public void DeleteButton()
+            {
+            NugetLogic RS = new();
+            var result = RS.GetList();
+            Reys mytest = new()
+            {
+                NumberReys = 253,
+                Plane = plane.Airbus,
+                arrivalTime = System.DateTime.Now,
+                NumberPassengers = 100,
+                NumberCrew = 2,
+                SborC = 1500,
+                SborP = 2000,
+                allowance = 3000,
+
+            };
+            RS.Add(mytest);
+            RS.Remove(mytest);
+            Assert.Empty(result);
+            }
+        
+    }
+}
